f3949dab794f637b28118b43d5d7923ce47818e5
luo0412 <1095847440@qq.com>
2016-10-16 20:51:08 +0800

    新增服务代码

--- Multimedia/app/src/main/java/com/example/multimedia/MainActivity.java
+++ Multimedia/app/src/main/java/com/example/multimedia/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends Activity implements OnClickListener {
     private Button choose_pic_btn;
     private Button play_audio_btn;
     private Button play_video_btn;
+    private Button thread_test_btn;
+    private Button service_test_btn;
+    private Button web_view_btn;
+//    private Button network_test_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,15 @@ public class MainActivity extends Activity implements OnClickListener {
         play_audio_btn.setOnClickListener(this);
         play_video_btn = (Button)findViewById(R.id.play_video_btn);
         play_video_btn.setOnClickListener(this);
+        thread_test_btn = (Button)findViewById(R.id.thread_test_btn);
+        thread_test_btn.setOnClickListener(this);
+        service_test_btn = (Button)findViewById(R.id.service_test_btn);
+        service_test_btn.setOnClickListener(this);
+        web_view_btn = (Button)findViewById(R.id.web_view_btn);
+        web_view_btn.setOnClickListener(this);
+//        network_test_btn = (Button)findViewById(R.id.network_test_btn);
+//        network_test_btn.setOnClickListener(this);
+
     }
 
     @Override
@@ -59,6 +72,26 @@ public class MainActivity extends Activity implements OnClickListener {
                 intent5.setClass(MainActivity.this, PlayVideoTestActivity.class);
                 startActivity(intent5);
                 break;
+            case R.id.thread_test_btn:
+                Intent intent6=new Intent();
+                intent6.setClass(MainActivity.this, ThreadTestActivity.class);
+                startActivity(intent6);
+                break;
+            case R.id.service_test_btn:
+                Intent intent7=new Intent();
+                intent7.setClass(MainActivity.this, ServiceTestActivity.class);
+                startActivity(intent7);
+                break;
+            case R.id.web_view_btn:
+                Intent intent8=new Intent();
+                intent8.setClass(MainActivity.this, WebViewActivity.class);
+                startActivity(intent8);
+                break;
+//            case R.id.network_test_btn:
+//                Intent intent9=new Intent();
+//                intent9.setClass(MainActivity.this, NetworkTestActivity.class);
+//                startActivity(intent9);
+//                break;
             default:
                 break;
         }
---
+++ Multimedia/app/src/main/java/com/example/multimedia/MyIntentService.java
@@ -0,0 +1,29 @@
+package com.example.multimedia;
+
+/**
+ * Created by Luo_0412 on 2016/10/16.
+ */
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyIntentService extends IntentService {
+
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d("MyIntentService", "Thread id is " + Thread.currentThread().getId());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("MyIntentService", "onDestroy executed");
+    }
+
+}
+
